df3e750f3b1cc8577c588f5efd2ab8a0fee7a151
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-28 15:38:10 +0200

    Add files via upload

--- mod4/index.js
+++ mod4/index.js
@@ -1,1 +1,16 @@
+import React, { Component, PropTypes } from 'react';
 
+class MultiModulesDemo extends Component {
+	
+  render(){
+	  
+    return (
+      <div className='FranklyModulesDemo'>
+	      <h2 className='demo_text'>mod2: {this.props.text}</h2>
+      </div>
+    );
+	  
+  }
+}
+
+export default ModuleDemo;
