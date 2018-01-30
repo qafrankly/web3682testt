a72a58ddaf6cae9f081e36dcdc66c7ee9099896d
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-30 15:44:53 +0200

    Update and rename mod1/index.js to nad1/index.js

--- nad1/index.js
+++ nad1/index.js
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
-class MultiModulesDemo11 extends Component {
+class MultiDemo11 extends Component {
 	
   render(){
 	  
     return (
       <div className='FranklyModulesDemo'>
-	      <h2 className='demo_text'>mod1: {this.props.text}</h2>
+	      <h2 className='demo_text'>nad1: {this.props.text}</h2>
       </div>
     );
 	  
   }
 }
 
-export default MultiModulesDemo11;
+export default MultiDemo11;
