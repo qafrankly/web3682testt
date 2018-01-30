5f2701f4b125f4060751473815381a50cd70a9fb
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-30 16:15:08 +0200

    Update and rename mod4/index.js to nad4/index.js

--- nad4/index.js
+++ nad4/index.js
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
-class MultiModulesDemo14 extends Component {
+class MultiDemo14 extends Component {
 	
   render(){
 	  
     return (
       <div className='FranklyFourthModulesDemo'>
-	      <h2 className='demo_text'>mod4: {this.props.text}</h2>
+	      <h2 className='demo_text'>nad4: {this.props.text}</h2>
       </div>
     );
 	  
   }
 }
 
-export default MultiModulesDemo14;
+export default MultiDemo14;
